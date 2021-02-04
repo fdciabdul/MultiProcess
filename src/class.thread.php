106014e24191455918cdb34fcb30b92c5c736d60
Abdul Muttaqin <abdulmuttaqin456@gmail.com>
2021-02-04 14:23:55 +0700

    KYAAAAA

---
+++ src/class.thread.php
@@ -0,0 +1,54 @@
+<?php
+    class Task
+    {
+        protected $generator;
+    
+        protected $run = false;
+    
+        public function __construct(Generator $generator)
+        {
+            $this->generator = $generator;
+        }
+    
+        public function run()
+        {
+            if ($this->run) {
+                $this->generator->next();
+            } else {
+                $this->generator->current();
+            }
+    
+            $this->run = true;
+        }
+    
+        public function finished()
+        {
+            return !$this->generator->valid();
+        }
+    }
+    class MultiThread
+    {
+        protected $queue;
+    
+        public function __construct()
+        {
+            $this->queue = new SplQueue();
+        }
+    
+        public function enqueue(Task $task)
+        {
+            $this->queue->enqueue($task);
+        }
+    
+        public function run()
+        {
+            while (!$this->queue->isEmpty()) {
+                $task = $this->queue->dequeue();
+                $task->run();
+            
+                if (!$task->finished()) {
+                    $this->enqueue($task);
+                }
+            }
+        }
+    }
