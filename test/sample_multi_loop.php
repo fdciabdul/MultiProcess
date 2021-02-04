b29c49937414a28c6af33a85c9a4873cf7a55588
Abdul Muttaqin <abdulmuttaqin456@gmail.com>
2021-02-04 14:24:41 +0700

    Create sample_multi_loop.php

---
+++ test/sample_multi_loop.php
@@ -0,0 +1,23 @@
+ $multi = new MultiThread();
+    
+    $task1 = new Task(call_user_func(function() {
+        for ($i = 0; $i < 3; $i++) {
+            print "task 1: " . $i . "\n";
+            yield;
+        }
+        // start by executing this
+    }));
+    
+    $task2 = new Task(call_user_func(function() {
+        for ($i = 0; $i < 6; $i++) {
+            print "task 2: " . $i . "\n";
+            yield;
+        }
+        // then start this one
+    }));
+    
+    $multi->enqueue($task1);
+    $multi->enqueue($task2);
+    
+    $multi->run();
+?>
